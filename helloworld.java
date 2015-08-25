9cda56802439f97bef7227e7544bd4ddd139f75c
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-25 14:36:56 -0700

    new comments

--- helloworld.java
+++ helloworld.java
@@ -7,3 +7,9 @@ public class Main {
 // static > will run without making an instance
 // void > will not return a value
 // main > method name
+/** 
+ *system > prefdefined class of methods/variables
+ *out > stdout static variable
+ *println > prints that line out
+ */
+ 
