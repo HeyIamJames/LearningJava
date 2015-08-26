11f6a0c7804a1a01b14bf0cd428286b83351fd1e
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-26 15:57:39 -0700

    Update reverselinkedlist.java

--- reverselinkedlist.java
+++ reverselinkedlist.java
@@ -14,3 +14,14 @@ recursiveReverse(currentNode.next);
 currentNode.next.next = currentNode;
 currentNode.next = null;
 }
+
+//another solution
+Node reversedPart = null;
+Node current = head;
+while (current != null) {
+    Node next = current.next;
+    current.next = reversedPart;
+    reversedPart = current;
+    current = next;
+}
+head = reversedPart;
