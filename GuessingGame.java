68014677d951bd0424f6d09c95ae958f7ed2858e
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-11-01 19:41:05 -0800

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,11 @@
+import java.util.Random;
+
 public class GuessingGame {
 
     public staic void main(String[] args) {
+        
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(1000);
         
     }
 }
