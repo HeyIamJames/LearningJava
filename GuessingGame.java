3183fb0fc1b849921462ac32d0a17f9b7cde1d78
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-11-02 23:56:42 -0800

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -7,6 +7,8 @@ public class GuessingGame {
         Random rand = new Random();
         int numberToGuess = rand.nextInt(1000);
         int numberOfTries = 0;
+        Scanner input = new Scanner (System.in);
+        int guess;
         
         System.out.println("Guess a number 1-1000")
         
